6b4a95f8df313de3ebc37eaf19db203b1216361a
hhgttg <ksatoshi119@gmail.com>
2025-12-03 11:40:25 +0900

    lispbuilder メインループ枠組み

--- main.lisp
+++ main.lisp
@@ -8,12 +8,19 @@
     (:quit-event() t)
     (:key-down-event
      (:key key)
-     (if  (sdl:key= key :sdl-key-escape )
-	  (sdl:push-quit-event)
-))))
-
+     (if (sdl:key= key :sdl-key-escape)
+	 (sdl:push-quit-event)
+	 (put-buffer key)))     ;*key-buffer*にenque
+    (:idle
+     ()
+     (format t "KEY in buffer : ~A~%" (get-buffer)))    ;*key-buffer*からdeque
+    (:video-expose-event
+     () ; 再描画などの処理
+     (sdl:update-display))))))
+	  
 (defun main()
   (sdl:with-init ()
     (sdl:window 640 480)
+    (setf (sdl:frame-rate) 60)
     (sdl:update-display)
     (main-loop)))
